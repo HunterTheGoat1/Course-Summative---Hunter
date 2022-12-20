617ae48ddc06ca7610abe38e303e1220af293b92
Hunter <WilsHunt604@gotvdsb.ca>
2022-12-20 09:39:19 -0500

    eahaeh

---
+++ Blade.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_Summative___Hunter
+{
+    internal class Blade
+    {
+        private Rectangle _rectangle;
+        private Texture2D _texture;
+        private Vector2 _speed;
+
+        public Blade(Rectangle rectangle, Vector2 speed, Texture2D texture)
+        {
+            _rectangle = rectangle;
+            _texture = texture;
+            _speed = speed;
+        }
+
+        public Texture2D Texture
+        {
+            get { return _texture; }
+        }
+
+        public Rectangle BoundRect
+        {
+            get { return _rectangle; }
+            set { _rectangle = value; }
+        }
+
+        public void Move(GraphicsDeviceManager graphics)
+        {
+            _rectangle.Offset(_speed);
+            if (_rectangle.Right > graphics.PreferredBackBufferWidth || _rectangle.Left < 0)
+            {
+                _speed.X *= -1;
+            }
+            if (_rectangle.Bottom > graphics.PreferredBackBufferHeight || _rectangle.Top < 0)
+            {
+                _speed.Y *= -1;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _rectangle, Color.White);
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -31,10 +31,17 @@ namespace Course_Summative___Hunter
         Texture2D shopTint;
         Texture2D swordIcon;
         Rectangle shopButton;
+        SpriteFont shopText;
+        Rectangle atkBuyRect;
+        Texture2D sawBlade;
+        Rectangle bladeBuyRect;
         double castleHealth;
         int coins;
+        int bladeCount;
         int atkDamage;
+        int wave;
         List<BasicEnemy> basicEnemys;
+        List<Blade> bladesList;
         Random ranGen;
         Screen screen;
         enum Screen
@@ -61,46 +68,18 @@ namespace Course_Summative___Hunter
             screen = Screen.MainMenu;
             castleHealth = 100;
             coins = 0;
+            wave = 0;
             atkDamage = 1;
+            bladeCount = 0;
             playButtonRect = new Rectangle(200, 530, 300, 150);
             castleRect = new Rectangle(275, 250, 150, 150);
             shopButton = new Rectangle(620, 5, 75, 75);
+            atkBuyRect = new Rectangle(125, 25, 50, 50);
+            bladeBuyRect = new Rectangle(200, 25, 50, 50);
             base.Initialize();
             ranGen = new Random();
             basicEnemys = new List<BasicEnemy>();
-
-            for (int i = 0; i < 25; i++)
-            {
-                Texture2D basicTexture = badGuyTextureDown;
-                int x = 0;
-                int y = 0;
-                int spawnSide = ranGen.Next(1, 5);
-                if (spawnSide == 1)
-                {
-                    x = ranGen.Next(0, 650);
-                    y = ranGen.Next(-300, 50);
-                    basicTexture = badGuyTextureDown;
-                }
-                if (spawnSide == 2)
-                {
-                    x = ranGen.Next(0, 650);
-                    y = ranGen.Next(600, 1000);
-                    basicTexture = badGuyTextureUp;
-                }
-                if (spawnSide == 3)
-                {
-                    x = ranGen.Next(-300, 50);
-                    y = ranGen.Next(0, 650);
-                    basicTexture = badGuyTextureRight;
-                }
-                if (spawnSide == 4)
-                {
-                    x = ranGen.Next(600, 1000);
-                    y = ranGen.Next(0, 650);
-                    basicTexture = badGuyTextureLeft;
-                }
-                basicEnemys.Add(new BasicEnemy(new Rectangle(x, y, 50, 50), basicTexture, 2));
-            }
+            bladesList = new List<Blade>();
         }
 
         protected override void LoadContent()
@@ -112,6 +91,7 @@ namespace Course_Summative___Hunter
             playButton = Content.Load<Texture2D>("playButton");
             castleHealthText = Content.Load<SpriteFont>("healthText");
             badGuyHealth = Content.Load<SpriteFont>("badGuyHealth");
+            shopText = Content.Load<SpriteFont>("shopText");
             badGuyTextureDown = Content.Load<Texture2D>("basicEnemyDown");
             badGuyTextureUp = Content.Load<Texture2D>("basicEnemyUp");
             badGuyTextureLeft = Content.Load<Texture2D>("basicEnemyLeft");
@@ -121,6 +101,7 @@ namespace Course_Summative___Hunter
             shopIcon = Content.Load<Texture2D>("shopIcon");
             swordIcon = Content.Load<Texture2D>("swordIcon");
             shopTint = Content.Load<Texture2D>("shopTint");
+            sawBlade = Content.Load<Texture2D>("sawBlade");
         }
 
         protected override void Update(GameTime gameTime)
@@ -139,10 +120,61 @@ namespace Course_Summative___Hunter
 
             else if (screen == Screen.GameScreen)
             {
+                if (basicEnemys.Count == 0)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Texture2D basicTexture = badGuyTextureDown;
+                        int x = 0;
+                        int y = 0;
+                        int spawnSide = ranGen.Next(1, 5);
+                        if (spawnSide == 1)
+                        {
+                            x = ranGen.Next(0, 650);
+                            y = ranGen.Next(-300, 50);
+                            basicTexture = badGuyTextureDown;
+                        }
+                        if (spawnSide == 2)
+                        {
+                            x = ranGen.Next(0, 650);
+                            y = ranGen.Next(600, 1000);
+                            basicTexture = badGuyTextureUp;
+                        }
+                        if (spawnSide == 3)
+                        {
+                            x = ranGen.Next(-300, 50);
+                            y = ranGen.Next(0, 650);
+                            basicTexture = badGuyTextureRight;
+                        }
+                        if (spawnSide == 4)
+                        {
+                            x = ranGen.Next(600, 1000);
+                            y = ranGen.Next(0, 650);
+                            basicTexture = badGuyTextureLeft;
+                        }
+                        basicEnemys.Add(new BasicEnemy(new Rectangle(x, y, 50, 50), basicTexture, 2));
+                    }
+                }
+
                 bool test = false;
+
                 for (int i = 0; i < basicEnemys.Count; i++)
                 {
                     castleHealth = basicEnemys[i].Move(_graphics, castleRect, castleHealth);
+                    foreach (Blade blade in bladesList)
+                    {
+                        if (!test && blade.BoundRect.Intersects(basicEnemys[i].BoundRect))
+                        {
+                            test = true;
+                            basicEnemys[i].Damage(1);
+                            if (basicEnemys[i].Health <= 0)
+                            {
+                                basicEnemys.RemoveAt(i);
+                                coins += ranGen.Next(1, 4);
+                                i--;
+                            }
+                        }
+                    }
                     // Detects a click on enemies, applies damage
                     if (!test && mouseState.LeftButton == ButtonState.Pressed && preMouseState.LeftButton == ButtonState.Released)
                     {
@@ -159,6 +191,10 @@ namespace Course_Summative___Hunter
                         }
                     }
                 }
+                foreach (Blade blade in bladesList)
+                {
+                    blade.Move(_graphics);
+                }
                 if (mouseState.LeftButton == ButtonState.Pressed && preMouseState.LeftButton == ButtonState.Released)
                 {
                     if (shopButton.Contains(mouseState.X, mouseState.Y))
@@ -174,6 +210,25 @@ namespace Course_Summative___Hunter
                     if (shopButton.Contains(mouseState.X, mouseState.Y))
                     {
                         screen = Screen.GameScreen;
+                    }
+                    if (atkBuyRect.Contains(mouseState.X, mouseState.Y))
+                    {
+                        int cost = atkDamage * 10;
+                        if (coins >= cost)
+                        {
+                            atkDamage++;
+                            coins -= cost;
+                        }
+                    }
+                    if (bladeBuyRect.Contains(mouseState.X, mouseState.Y))
+                    {
+                        int cost = (bladeCount * 100) + 100;
+                        if (coins >= cost)
+                        {
+                            bladeCount++;
+                            bladesList.Add(new Blade(new Rectangle(ranGen.Next(100, 600), ranGen.Next(100, 600), 30, 30), new Vector2(-2, 2), sawBlade));
+                            coins -= cost;
+                        }
                     }
                 }
             }
@@ -209,9 +264,15 @@ namespace Course_Summative___Hunter
                 _spriteBatch.Draw(shopIcon, shopButton, Color.White);
                 _spriteBatch.Draw(swordIcon, new Rectangle(0, 64, 22, 22), Color.White);
                 _spriteBatch.DrawString(castleHealthText, $"{atkDamage}", new Vector2(30, 60), Color.LightGray);
+                _spriteBatch.Draw(sawBlade, new Rectangle(0, 94, 22, 22), Color.White);
+                _spriteBatch.DrawString(castleHealthText, $"{bladeCount}", new Vector2(30, 90), Color.LightGray);
                 foreach (BasicEnemy enemyB in basicEnemys)
                 {
                     enemyB.Draw(_spriteBatch, badGuyHealth);
+                }
+                foreach (Blade blade in bladesList)
+                {
+                    blade.Draw(_spriteBatch);
                 }
             }
 
@@ -226,11 +287,30 @@ namespace Course_Summative___Hunter
                 _spriteBatch.Draw(shopIcon, shopButton, Color.White);
                 _spriteBatch.Draw(swordIcon, new Rectangle(0, 64, 22, 22), Color.White);
                 _spriteBatch.DrawString(castleHealthText, $"{atkDamage}", new Vector2(30, 60), Color.LightGray);
+                _spriteBatch.Draw(sawBlade, new Rectangle(0, 94, 22, 22), Color.White);
+                _spriteBatch.DrawString(castleHealthText, $"{bladeCount}", new Vector2(30, 90), Color.LightGray);
                 foreach (BasicEnemy enemyB in basicEnemys)
                 {
                     enemyB.Draw(_spriteBatch, badGuyHealth);
                 }
+                foreach (Blade blade in bladesList)
+                {
+                    blade.Draw(_spriteBatch);
+                }
                 _spriteBatch.Draw(shopTint, new Rectangle(100, 0, 520, 100), Color.White);
+                _spriteBatch.Draw(swordIcon, atkBuyRect, Color.White);
+                _spriteBatch.DrawString(shopText, $"{atkDamage} -> {atkDamage + 1}", new Vector2(125, 0), Color.LightGray);
+                if (coins >= atkDamage * 10)
+                    _spriteBatch.DrawString(shopText, $"${atkDamage * 10}", new Vector2(125, 76), Color.Green);
+                else
+                    _spriteBatch.DrawString(shopText, $"${atkDamage * 10}", new Vector2(125, 76), Color.Red);
+
+                _spriteBatch.Draw(sawBlade, bladeBuyRect, Color.White);
+                _spriteBatch.DrawString(shopText, $"{bladeCount} -> {bladeCount + 1}", new Vector2(200, 0), Color.LightGray);
+                if (coins >= (bladeCount * 100) + 100)
+                    _spriteBatch.DrawString(shopText, $"${(bladeCount * 100) + 100}", new Vector2(200, 76), Color.Green);
+                else
+                    _spriteBatch.DrawString(shopText, $"${(bladeCount * 100) + 100}", new Vector2(200, 76), Color.Red);
             }
 
             else if (screen == Screen.EndScreen)
